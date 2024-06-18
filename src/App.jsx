5ae5bed30113375459841f22da35a69908a43734
chowankumar <chowanmalhani5@gmail.com>
2024-06-18 15:31:44 +0500

    upload pics and add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
  import React from 'react'
+import Todo from './components/Todo'
  
  const App = () => {
    return (
-     <div  className=''>  </div>
+     <div  className=''>
+      <Todo/>
+     </div>
    )
  }
  
