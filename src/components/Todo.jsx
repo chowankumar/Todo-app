32086d24f4b03d824090669c0bb809836d7ec3ef
chowankumar <chowanmalhani5@gmail.com>
2024-06-20 02:38:15 +0500

    add useRef

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,6 +3,8 @@ import todo_icon from "../assets/todo_icon.png"
 import TodoItem from './TodoItem'
 
 const Todo = () => {
+
+  const inputRef = useRef();
   return (
     <div className='bg-white place-self-center min-h-[550px] max-w-md w-11/12 flex flex-col p-7 rounded-xl'>
 
@@ -13,7 +15,7 @@ const Todo = () => {
      </div>
 
      <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-      <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task'/>
+      <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task'/>
       <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD + </button>
      </div>
 
